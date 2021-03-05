f8009e6d02e9b34505109c0c022e0dc05a846772
Shawn Nichol <7067shawnnichol@gmail.com>
2021-03-04 22:26:20 -0700

    Initial commit

---
+++ app/src/main/java/com/example/cl_room_words/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.cl_room_words
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
