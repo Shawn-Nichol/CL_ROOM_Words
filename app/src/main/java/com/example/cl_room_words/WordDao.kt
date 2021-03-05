431c1e81ae316dbcdfdd567eff3188f28c8d6a4c
Shawn Nichol <7067shawnnichol@gmail.com>
2021-03-04 22:43:41 -0700

    6) DAO

---
+++ app/src/main/java/com/example/cl_room_words/WordDao.kt
@@ -0,0 +1,25 @@
+package com.example.cl_room_words
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+
+
+/**
+ * Dao is an interface: DAO must either be an interface or abstract class.
+ * @DAO: identifies it as a DAO class for Room
+ */
+@Dao
+interface WordDao {
+    @Query("SELECT * FROM word_table ORDER BY word ASC")
+    fun getAlphabetizedWords(): List<Word>
+
+    // @Insert: is special DAO method annotation where you don't have to provide any SQL.
+    // onConflictStrategy: ignores matching words.
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(word: Word)
+
+    @Query("DELETE FROM word_table")
+    suspend fun deleteAll()
+}
