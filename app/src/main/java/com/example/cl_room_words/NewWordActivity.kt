9261fc163df1275915613d67c23d8a75e3dec632
Shawn Nichol <7067shawnnichol@gmail.com>
2021-03-08 22:25:23 -0700

    DataBinding added to NewWordActivity.kt

--- app/src/main/java/com/example/cl_room_words/NewWordActivity.kt
+++ app/src/main/java/com/example/cl_room_words/NewWordActivity.kt
@@ -7,31 +7,36 @@ import android.os.Bundle
 import android.text.TextUtils
 import android.widget.Button
 import android.widget.EditText
+import androidx.databinding.DataBindingUtil
+import com.example.cl_room_words.databinding.ActivityNewWordBinding
 
 class NewWordActivity : AppCompatActivity() {
 
     private lateinit var editWordView: EditText
 
+    private lateinit var binding: ActivityNewWordBinding
+
     public override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_new_word)
-        editWordView = findViewById(R.id.edit_word)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_new_word)
+        binding.binding = this
+        editWordView = binding.editWord
 
-        val button = findViewById<Button>(R.id.button_save)
-        button.setOnClickListener {
-            val replyIntent = Intent()
-            if (TextUtils.isEmpty(editWordView.text)) {
-                setResult(Activity.RESULT_CANCELED, replyIntent)
-            } else {
-                val word = editWordView.text.toString()
-                replyIntent.putExtra(EXTRA_REPLY, word)
-                setResult(Activity.RESULT_OK, replyIntent)
-            }
-            finish()
-        }
     }
 
     companion object {
         const val EXTRA_REPLY = "com.example.android.wordlistsql.REPLY"
+    }
+
+    fun saveWord() {
+        val replyIntent = Intent()
+        if (TextUtils.isEmpty(editWordView.text)) {
+            setResult(Activity.RESULT_CANCELED, replyIntent)
+        } else {
+            val word = editWordView.text.toString()
+            replyIntent.putExtra(EXTRA_REPLY, word)
+            setResult(Activity.RESULT_OK, replyIntent)
+        }
+        finish()
     }
 }
